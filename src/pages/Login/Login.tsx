e0827b5262987c0d6dd5503b5511bb4a5d011538
Thiago Lessa Do Nascimento <tlessa@cpqd.com.br>
2024-07-15 08:34:12 -0300

    estilizacao tela de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import loginStyle from "./style";
-// import Logo from "../../assets/barber-logo.png";
 
 const Login = () => {
   return (
     <View style={loginStyle.container}>
-      {/* <Image style={loginStyle.image} source={Logo}></Image> */}
-      <Text> Login</Text>
+      <View style={loginStyle.inputContainer}>
+        <Image
+          // esse estilo diferente é para transformar a imagem png preta em branca só com css
+          style={[loginStyle.image, { tintColor: "white" }]}
+          source={require("../../assets/barber-logo.png")}
+        />
+        <TextInput
+          style={loginStyle.input}
+          placeholder="Insira seu email"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={loginStyle.input}
+          placeholder="Insira sua senha"
+          placeholderTextColor="white"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={loginStyle.button}>
+          <Text style={{ color: "white" }}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={loginStyle.createAccountContainer}>
+        <TouchableOpacity>
+          <Text style={{ color: "white" }}>Criar uma conta</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -3,12 +3,44 @@ import { StyleSheet } from "react-native";
 const loginStyle = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#626262",
     alignItems: "center",
     justifyContent: "center",
   },
   image: {
-    width: 10,
+    width: "100%",
+    height: 100,
+  },
+  inputContainer: {
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    flex: 1,
+  },
+  input: {
+    backgroundColor: "#383838",
+    width: "100%",
+    color: "white",
+    padding: 20,
+    borderRadius: 8,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#191919",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  createAccountContainer: {
+    flex: 0.1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#191919",
+    width: "100%",
   },
 });
 
