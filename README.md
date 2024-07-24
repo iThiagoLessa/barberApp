c5742500e29e761a63e2493370e97d9e39d0473f
Thiago Lessa Do Nascimento <tlessa@cpqd.com.br>
2024-07-24 14:32:26 -0300

    adicionando readMe

---
+++ README.md
@@ -0,0 +1,3 @@
+# This a preview of the readme
+
+I'm building this app using react native for my studies
---
+++ src/pages/Appointments/components/SelectedBarber/SelectedBarber.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const SelectedBarber = () => {
+  return <Text>SelectedBarber</Text>;
+};
+
+export default SelectedBarber;
